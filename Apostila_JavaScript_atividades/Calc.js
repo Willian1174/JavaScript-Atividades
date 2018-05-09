f95c827f16331ffca256ea1147376bd93793fb32
willian1174 <willianrodrigues273@gmail.com>
2018-05-09 13:45:32 -0300

    Objeto implementado com 4 operações básicas

---
+++ Apostila_JavaScript_atividades/Calc.js
@@ -0,0 +1,31 @@
+
+
+ var calc = {
+
+	soma:function(a1,a2) {
+
+    return a1+a2;
+
+	},
+
+	subtracao:function(a1,a2) {
+
+    return a1-a2;
+
+	},
+
+    divisao:function(a1,a2) {
+
+    return a1/a2;
+
+	},
+    
+    multi:function(a1,a2) {
+
+    return a1*a2;
+
+	},
+
+
+
+}
