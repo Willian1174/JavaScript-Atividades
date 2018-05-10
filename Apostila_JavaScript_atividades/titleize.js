7472c22a426b48ae5bf3e74ad3ac440308405d87
Willian1174 <willianrodrigues273@gmail.com>
2018-05-10 14:20:53 -0300

    Deixar todas as primeiras letras maiúsculas
    Identação

--- Apostila_JavaScript_atividades/titleize.js
+++ Apostila_JavaScript_atividades/titleize.js
@@ -6,14 +6,14 @@ function titleize(text){
 	var palavra = text.split(' ')
 
 	var obj = {
-        
+		
 
 	}
-	 for (var i = 0; i < palavra.length; i++){
+	for (var i = 0; i < palavra.length; i++){
 
-	  obj[palavra[i]]=palavra[i].substring(0,1).toUpperCase()+palavra[i].substring(1,palavra[i].split('').length)
-      resultado+=' '+obj[palavra[i]]
-      
-	 }
-return console.log(resultado)
+		obj[palavra[i]]=palavra[i].substring(0,1).toUpperCase()+palavra[i].substring(1,palavra[i].split('').length)
+		resultado+=' '+obj[palavra[i]]
+		
+	}
+	return console.log(resultado)
 }
