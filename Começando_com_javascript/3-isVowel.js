4f2fce387df2c59c195becd8b66b40f3d91fed62
Willian1174 <willianrodrigues273@gmail.com>
2018-08-06 21:28:33 -0300

    Atualização do repositório

--- Começando_com_javascript/3-isVowel.js
+++
@@ -1,9 +0,0 @@
-//'Definindo se a letra é uma vogal ou consoante
-
-function isVowel(letter){
-
-var search = /[aeiou]/ig;
-
-search.test(letter) ? document.write("É uma vogal") : document.write("É uma consoante")
-
-}
